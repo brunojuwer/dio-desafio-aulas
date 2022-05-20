ae3589dbb219e3a306b3fee82e0f216098d76e33
brunojuwer <bruno.juwer1997@gmail.com>
2022-05-20 14:49:20 -0300

    Atividades sobre coleções

---
+++ Módulo 3 - JavaScript/4 - Coleções/maps/app.js
@@ -0,0 +1,19 @@
+function getAdmins(map) {
+    let admins = [];
+
+    for([key, value] of map) {
+        if(value === 'Admin'){
+            admins.push(key)
+        }
+    }
+    return admins
+}
+
+const usuarios = new Map();
+
+usuarios.set('Luiz', 'Admin');
+usuarios.set('Bruno', 'Admin');
+usuarios.set('Jorge', 'User');
+usuarios.set('Ana', 'Admin');
+
+console.log(getAdmins(usuarios));
