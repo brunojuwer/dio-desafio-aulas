22fbce09059ff6c7e5725ee43691ad97b6d29b36
brunojuwer <bruno.juwer1997@gmail.com>
2022-05-24 08:55:14 -0300

    Atividade de módulos em JS

---
+++ Módulo 3 - JavaScript/9 - Módulos/Atividade/main.mjs
@@ -0,0 +1,7 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Bruno", 24));
+console.log(mostraCidade("Bruno", "Alto Feliz"));
+console.log(mostraHobby("Bruno", "Drums"));
+
+
